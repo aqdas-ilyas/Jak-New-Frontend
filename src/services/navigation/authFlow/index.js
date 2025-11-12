39807e994166185cedda84180e20b0c770c93547
aqdas ilyas <aqdus5040@gmail.com>
2025-11-12 11:51:49 +0500

    Remove the Padding Contanier style from Splash Screne

--- src/services/navigation/authFlow/index.js
+++ src/services/navigation/authFlow/index.js
@@ -11,28 +11,28 @@ const AuthStack = createNativeStackNavigator();
 
 export const AuthNavigation = () => {
   const insets = useSafeAreaInsets();
-  const contentStyle = useMemo(() => ({
+  const paddedContentStyle = useMemo(() => ({
     backgroundColor: colors.fullWhite,
     paddingBottom: insets.bottom > 0 ? insets.bottom : wp(5),
   }), [insets.bottom]);
 
   return (
-    <AuthStack.Navigator screenOptions={{ headerShown: false, gestureEnabled: false, contentStyle }}>
-      <AuthStack.Screen name={routes.splash} component={Auth.Splash} />
-      <AuthStack.Screen name={routes.onboard} component={Auth.Onboarding} />
-      <AuthStack.Screen name={routes.welcome} component={Auth.Welcome} />
-      <AuthStack.Screen name={routes.login} component={Auth.SignIn} />
-      <AuthStack.Screen name={routes.register} component={Auth.SignUp} />
-      <AuthStack.Screen name={routes.forgotPassword} component={Auth.ForgetPassword} />
-      <AuthStack.Screen name={routes.resetPassword} component={Auth.ResetPassword} />
-      <AuthStack.Screen name={routes.otp} component={Auth.SendOtp} />
-      <AuthStack.Screen name={routes.createProfile} component={Auth.CreateProfile} />
-      <AuthStack.Screen name={routes.preferences} component={Auth.Preferences} />
-      <AuthStack.Screen name={routes.subscription} component={Auth.Subscription} />
-      <AuthStack.Screen name={routes.payment} component={Auth.Payment} />
-      <AuthStack.Screen name={routes.card} component={Auth.Card} />
-      <AuthStack.Screen name={routes.privacyPolicy} component={App.PrivacyPolicy} />
-      <AuthStack.Screen name={routes.termsConditions} component={App.TermsConditions} />
+    <AuthStack.Navigator screenOptions={{ headerShown: false, gestureEnabled: false }}>
+      <AuthStack.Screen name={routes.splash} component={Auth.Splash} options={{ contentStyle: { backgroundColor: colors.fullWhite } }} />
+      <AuthStack.Screen name={routes.onboard} component={Auth.Onboarding} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.welcome} component={Auth.Welcome} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.login} component={Auth.SignIn} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.register} component={Auth.SignUp} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.forgotPassword} component={Auth.ForgetPassword} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.resetPassword} component={Auth.ResetPassword} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.otp} component={Auth.SendOtp} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.createProfile} component={Auth.CreateProfile} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.preferences} component={Auth.Preferences} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.subscription} component={Auth.Subscription} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.payment} component={Auth.Payment} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.card} component={Auth.Card} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.privacyPolicy} component={App.PrivacyPolicy} options={{ contentStyle: paddedContentStyle }} />
+      <AuthStack.Screen name={routes.termsConditions} component={App.TermsConditions} options={{ contentStyle: paddedContentStyle }} />
     </AuthStack.Navigator>
   );
 };
