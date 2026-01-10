3074af475e18270684b120f8837a1ddb2c93ade3
aqdas ilyas <aqdus5040@gmail.com>
2026-01-10 22:49:04 +0500

    upgrade Android Version to 0.82.1. (16KB Support Now)

---
+++ android/app/.cxx/RelWithDebInfo/1m6y3151/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "/Users/aqdas/.gradle/caches/9.0.0/transforms/8b8ca282681f93672b4094299c7db947/transformed/jetified-hermes-android-0.82.1-release/prefab/modules/hermesvm/libs/android.x86/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/aqdas/.gradle/caches/9.0.0/transforms/8b8ca282681f93672b4094299c7db947/transformed/jetified-hermes-android-0.82.1-release/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
